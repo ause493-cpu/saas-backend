196f2c027d8d46ac645a16b9dd12c6448b936346
ause493-cpu <ause493@expert.micro1.ai>
2026-07-22 14:25:54 -0300

    Delete db/migrations/20260722_add_subscription_tier_to_users.sql

--- db/migrations/20260722_add_subscription_tier_to_users.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add subscription_tier column to users table
--- Ref: Notion task "Feature: Add Subscription Tier"
--- https://app.notion.com/p/396d473a25a480389bd9f4a1a2cfcdcc
-
-ALTER TABLE users
-ADD COLUMN subscription_tier VARCHAR(50) NOT NULL DEFAULT 'free';
